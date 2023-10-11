4d60611b3bc97bed0a242311a0a1a03c40f2625e
Neamul01 <neamulhaque2002@gmail.com>
2023-10-11 14:41:14 +0600

    distracture

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>HomeScreen </Text>
       <Button
-        onPress={() => props.navigation.navigate("List")}
+        onPress={() => navigation.navigate("Component")}
         title="Got to components demo"
       />
-      <TouchableOpacity onPress={() => props.navigation.navigate("Component")}>
-        <Text>Go to list</Text>
-      </TouchableOpacity>
+      <Button title="Go to List" onPress={() => navigation.navigate("List")} />
     </View>
   );
 };
